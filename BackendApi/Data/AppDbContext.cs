02ab01e1113ae434a98e27b006804ab4e6b6d497
atasoyturk <atasoyturkkk@hotmail.com>
2026-07-01 13:41:37 +0300

    added spatial DbSets to AppDbContext

--- BackendApi/Data/AppDbContext.cs
+++ BackendApi/Data/AppDbContext.cs
@@ -8,4 +8,8 @@ public sealed class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<User> Users => Set<User>();
+
+    public DbSet<PointEntity>   Points   => Set<PointEntity>();
+    public DbSet<LineEntity>    Lines    => Set<LineEntity>();
+    public DbSet<PolygonEntity> Polygons => Set<PolygonEntity>();
 }

3c0099827cd77d90a3a1763a28eafa267adc678b
atasoyturk <atasoyturkkk@hotmail.com>
2026-07-01 12:04:40 +0300

    added ExistsByEmail and AddAsync to IUserRepository

--- BackendApi/Repositories/IUserRepository.cs
+++ BackendApi/Repositories/IUserRepository.cs
@@ -5,4 +5,6 @@ namespace BackendApi.Repositories;
 public interface IUserRepository
 {
     Task<User?> GetByEmailAsync(string email);
+    Task<bool> ExistsByEmailAsync(string email);  
+    Task AddAsync(User user);     
 }

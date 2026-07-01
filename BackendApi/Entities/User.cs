4ed88cfb87e92342acb6e9236e8ac020ded435dd
atasoyturk <atasoyturkkk@hotmail.com>
2026-07-01 13:40:18 +0300

    added IsActive, IsDeleted, ModifiedDate to User

--- BackendApi/Entities/User.cs
+++ BackendApi/Entities/User.cs
@@ -6,4 +6,8 @@ public sealed class User
     public string Email        { get; init; } = string.Empty;
     public string PasswordHash { get; init; } = string.Empty;
     public string Role         { get; init; } = string.Empty;
+
+    public bool     IsActive      { get; set; } = true;
+    public bool     IsDeleted     { get; set; } = false;
+    public DateTime ModifiedDate  { get; set; } = DateTime.UtcNow;
 }

29c3f4e0758c738743f1d30647096a7f9e6d8c58
atasoyturk <atasoyturkkk@hotmail.com>
2026-07-02 11:46:05 +0300

    added Name and Color to GeoRequestDto

--- BackendApi/DTOs/GeoRequestDto.cs
+++ BackendApi/DTOs/GeoRequestDto.cs
@@ -6,4 +6,6 @@ public sealed class GeoRequestDto
 {
     [Required, MinLength(1)]
     public string WktGeometry { get; init; } = string.Empty;
+    public string Name { get; init; } = string.Empty;
+    public string Color { get; init; } = string.Empty;
 }

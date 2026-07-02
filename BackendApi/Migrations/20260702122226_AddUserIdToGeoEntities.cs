3ac1cb5465c459aaa8bfdf034122c36af1f768d9
atasoyturk <atasoyturkkk@hotmail.com>
2026-07-02 15:32:56 +0300

    Added UserId to geo entity tables migration

---
+++ BackendApi/Migrations/20260702122226_AddUserIdToGeoEntities.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserIdToGeoEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "tbl_polygon",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "tbl_point",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "tbl_line",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "tbl_polygon");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "tbl_point");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "tbl_line");
+        }
+    }
+}

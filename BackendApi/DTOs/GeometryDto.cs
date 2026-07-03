cf41fa4ac6ad2a3beecac35b9e2f50606109ead0
atasoyturk <atasoyturkkk@hotmail.com>
2026-07-03 11:58:33 +0300

    Removed unused GeometryDto file

--- BackendApi/DTOs/GeometryDto.cs
+++
@@ -1,5 +0,0 @@
-namespace BackendApi.DTOs;
-
-public sealed record PointDto(int Id, string WktGeometry);
-public sealed record LineDto(int Id, string WktGeometry);
-public sealed record PolygonDto(int Id, string WktGeometry);

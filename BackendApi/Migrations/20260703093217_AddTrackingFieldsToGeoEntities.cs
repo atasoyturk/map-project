66292cdea5c202ee57ab8af5005a01efeb062568
atasoyturk <atasoyturkkk@hotmail.com>
2026-07-03 12:35:12 +0300

    Added tracking fields migration for geo entities

---
+++ BackendApi/Migrations/20260703093217_AddTrackingFieldsToGeoEntities.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTrackingFieldsToGeoEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "tbl_polygon",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "tbl_polygon",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "tbl_polygon",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "tbl_point",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "tbl_point",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "tbl_point",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "tbl_line",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "tbl_line",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "tbl_line",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "tbl_polygon");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "tbl_polygon");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "tbl_polygon");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "tbl_point");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "tbl_point");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "tbl_point");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "tbl_line");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "tbl_line");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "tbl_line");
+        }
+    }
+}

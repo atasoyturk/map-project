b6fb7f40950ca056805a54c83b7b8fdeaf6cde98
atasoyturk <atasoyturkkk@hotmail.com>
2026-07-06 15:02:23 +0300

    Added CreatedDate to geo response DTOs

--- BackendApi/DTOs/GeoResponseDto.cs
+++ BackendApi/DTOs/GeoResponseDto.cs
@@ -1,21 +1,23 @@
 namespace BackendApi.DTOs;
 
 public sealed record PointResponseDto(
-    int    Id, 
-    string Name, 
-    string Color, 
-    string WktGeometry);
+    int      Id,
+    string   Name,
+    string   Color,
+    string   WktGeometry,
+    DateTime CreatedDate);   
 
 public sealed record LineResponseDto(
-    int    Id, 
-    string Name, 
-    string Color, 
-    string WktGeometry);
+    int      Id,
+    string   Name,
+    string   Color,
+    string   WktGeometry,
+    DateTime CreatedDate);   
 
 public sealed record PolygonResponseDto(
-    int    Id,
-    string Name,
-    string Color,
-    string WktGeometry,
-    int    IntersectedInventoryCount   
-);
+    int      Id,
+    string   Name,
+    string   Color,
+    string   WktGeometry,
+    int      IntersectedInventoryCount,
+    DateTime CreatedDate);   

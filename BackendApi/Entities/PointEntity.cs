284b741a78e08d80f911617c98c97fd10a1cdfc8
atasoyturk <atasoyturkkk@hotmail.com>
2026-07-07 11:56:22 +0300

    Removed base attributes, inherits from BaseEntity now

--- BackendApi/Entities/PointEntity.cs
+++ BackendApi/Entities/PointEntity.cs
@@ -4,16 +4,10 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace BackendApi.Entities;
 
 [Table("tbl_point")]
-public sealed class PointEntity
+public sealed class PointEntity : BaseEntity
 {
-    public int      Id           { get; init; }
-    public Geometry Geometry     { get; set; } = null!;
-    public string   Name         { get; set; } = string.Empty;  
-    public string   Color        { get; set; } = string.Empty; 
-    public int      UserId       { get; set; } 
-    public bool     IsActive     { get; set; } = true;   
-    public bool     IsDeleted    { get; set; } = false;  
-    public DateTime CreatedDate  { get; set; } = DateTime.UtcNow;  
-    public DateTime ModifiedDate { get; set; } = DateTime.UtcNow; 
-    public int?     ModifiedUserId { get; set; } 
+    public Geometry  Geometry       { get; set; } = null!;
+    public string    Name           { get; set; } = string.Empty;
+    public string    Color          { get; set; } = string.Empty;
+    public int       UserId         { get; set; }
 }

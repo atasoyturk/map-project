18b3c4cbdb3c9f37cefe37d058ee0268ea9e87be
atasoyturk <atasoyturkkk@hotmail.com>
2026-07-07 12:39:03 +0300

    Registered permission policies and PermissionHandler in DI

--- BackendApi/Program.cs
+++ BackendApi/Program.cs
@@ -3,11 +3,13 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Tokens;
 using BackendApi.Services;
 using BackendApi.Settings;
 using BackendApi.Data;
 using BackendApi.Repositories;
+using BackendApi.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -50,7 +52,18 @@ builder.Services
         };
     });
 
-builder.Services.AddAuthorization();
+// Authorization
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("admin_access",   policy =>
+        policy.Requirements.Add(new PermissionRequirement("admin_access")));
+    options.AddPolicy("point_create",   policy =>
+        policy.Requirements.Add(new PermissionRequirement("point_create")));
+    options.AddPolicy("line_create",    policy =>
+        policy.Requirements.Add(new PermissionRequirement("line_create")));
+    options.AddPolicy("polygon_create", policy =>
+        policy.Requirements.Add(new PermissionRequirement("polygon_create")));
+});
 
 // Application Services 
 builder.Services.AddScoped<ITokenService, JwtTokenService>();
@@ -59,6 +72,8 @@ builder.Services.AddScoped<ILineService,    LineService>();
 builder.Services.AddScoped<IPolygonService, PolygonService>();
 builder.Services.AddScoped<IAnalysisService, AnalysisService>();
 builder.Services.AddScoped<ISearchService, SearchService>();
+builder.Services.AddScoped<IAuthorizationHandler, PermissionHandler>();
+builder.Services.AddScoped<IPermissionService,    PermissionService>();
 
 builder.Services.AddControllers();
 
